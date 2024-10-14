995bd079d2fcb416eab360b3ab0f9f68c2c1b16e
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-10-14 10:37:50 +0300

    Repeating Task

---
+++ remove_element/main_2nd.py
@@ -0,0 +1,11 @@
+nums = [3, 2, 2, 3]
+val = 3
+
+k = 0
+for i in range(len(nums)):  # Adjust the range to avoid out-of-bounds error
+    if nums[i] != val:
+        nums[k] = nums[i]
+        k += 1
+
+
+print(k)
